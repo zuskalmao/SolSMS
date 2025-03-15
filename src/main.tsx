87088fc208682fcbca039f774a9c6ac2586da0cc
zuskalmao <zuskamalovich@gmail.com>
2025-03-15 00:01:38 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// IMPORTANT: Buffer polyfill for browser environment
+import { Buffer } from 'buffer';
+// Make Buffer available globally
+window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
-)
+);
