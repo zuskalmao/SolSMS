5f6df93d103d541b2325eff14c6c4fb3448b2086
zuskalmao <zuskamalovich@gmail.com>
2025-05-27 20:05:02 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -97,7 +97,7 @@ const Hero = () => {
           {/* Hero Image */}
           <div className="relative z-10 rounded-2xl overflow-hidden shadow-2xl border border-primary/20 glow">
             <img 
-              src="https://ipfs.io/ipfs/QmTapigXYgfE6B9vdpP42LdDDAebqQnSNxjjsEEKvJb6Df" 
+              src="https://ipfs.io/ipfs/QmV2WZjYCnzFGz5K9QmGKGk3cZhhztqt67nrTUpwCZ1nrP" 
               alt="Solana NFT Messaging Platform" 
               className="w-full h-auto object-cover"
             />
